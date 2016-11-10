d4292930ab8a7126e0d25a20ec250a213f1a0d21
RYANBROPHY <BROPHY2321@ME.COM>
2016-11-10 14:51:12 -0500

    node index.js works

---
+++ index.js
@@ -0,0 +1,10 @@
+var connect = require('express')();
+var http = require('http').Server(connect);
+
+connect.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
